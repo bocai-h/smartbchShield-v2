f1cdeed070e3a2cc221bdefc8bd48272e16d6030
bocai <woaichibocai_hxb@163.com>
2021-01-07 01:42:08 +0800

    update

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Col} from 'antd';
 import './index.less';
-
+import Web3 from 'web3';
+var Contract = require('web3-eth-contract');
 class Dashboard extends React.Component {
   state = {
     currentETHDeposited: 0,
@@ -16,8 +17,28 @@ class Dashboard extends React.Component {
     super(props);
   }
   async componentDidMount() {
-   
+   this.getCurrentETHDeposited();
+   this.getCurrentStableCoinsDeposited();
   }
+  async getCurrentETHDeposited() {
+    let newWeb3 = new Web3(window.web3.currentProvider);
+    let balanceWithDecimal = await newWeb3.eth.getBalance(SUTER_ETH_CONTRACT_ADDRESS);
+    let balance = newWeb3.utils.fromWei(balanceWithDecimal, 'ether');
+    this.setState({ currentETHDeposited: balance });
+  }
+
+  async getCurrentStableCoinsDeposited() {
+    let totalValue = 0;
+    let pools = [[SUTER_USDT_CONTRACT_ADDRESS, USDT_TOKEN_CONTRACT_ADDRESS, USDT_TOKEN_CONTRACT_ABI], [SUTER_DAI_CONTRACT_ADDRESS, DAI_TOKEN_CONTRACT_ADDRESS, DAI_TOKEN_CONTRACT_ABI], [SUTER_SUTER_CONTRACT_ADDRESS, SUTER_TOKEN_CONTRACT_ADDRESS, SUTER_TOKEN_CONTRACT_ABI]];
+    for (const item of pools) {
+      var suterShiledTokenContract = new Contract(item[2],item[1]);
+      suterShiledTokenContract.setProvider(window.web3.currentProvider);
+      let balanceWithDecimal = await suterShiledTokenContract.methods.balanceOf(item[0]).call();
+      totalValue += balanceWithDecimal;
+     }
+    this.setState({ currentStableCoinsDeposited: totalValue });
+  }
+
   render() {
     let { currentETHDeposited, currentStableCoinsDeposited, totalFeesUSD, totalETHDeposited, totalDeposits, totalUSDDeposited, totalUsers }  = this.state
     return (
