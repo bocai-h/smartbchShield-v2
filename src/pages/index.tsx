61e113e30bd3171c5f11579f1d0070e6edd10632
mynameis9527 <huanandli@icloud.com>
2021-01-16 21:27:11 +0800

    feat: translate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Layout } from 'antd';
 const { Header, Footer, Content } = Layout;
 import { Button, Menu, Dropdown } from 'antd';
 import intl from 'react-intl-universal';
-import { openNotificationWithIcon, ethChainNameMap } from '../components/tools';
 import 'antd/dist/antd.css';
 import Logo from '../static/suterShield.svg';
 import mLogo from '../static/logo.svg';
@@ -14,10 +13,6 @@ import twitterLight from '../static/twitter_light.svg';
 import telegramLight from '../static/telegram_light.svg';
 import mediumLight from '../static/medium_light.svg';
 import { MenuOutlined } from '@ant-design/icons';
-
-import Home from '../components/home';
-import Form from '../components/form';
-import ConnectModal from '../components/connectModal';
 
 const locales = {
   'en-US': require('../locales/en_US'),
@@ -113,51 +108,26 @@ class Portal extends Component {
 
     const menu1 = (
       <Menu>
-        <Menu.Item key={`q&a`}>
-          <a target="_blank" rel="noopener noreferrer" href="/qa">
-            {intl.get('Q&A')}
+        <Menu.Item key="transfer">
+          <a target="_blank" rel="noopener noreferrer" href="/transfer">
+            {intl.get('Transfer')}
           </a>
         </Menu.Item>
-        <Menu.Item key="dashboard">
-          <a target="_blank" rel="noopener noreferrer" href="/dashboard">
-            {intl.get('Dashboard')}
+        <Menu.Item key="mining">
+          <a target="_blank" rel="noopener noreferrer" href="/mining">
+            {intl.get('Mining')}
           </a>
         </Menu.Item>
-        <Menu.Item key="compliance">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/suterusu/regulation-compliance-of-suterusu-625abc752eb9"
-          >
-            {intl.get('Compliance')}
+        <Menu.Item key="tutorial">
+          <a target="_blank" rel="noopener noreferrer" href="/tutorial">
+            {intl.get('Tutorial')}
           </a>
         </Menu.Item>
-        <Menu.ItemGroup title={intl.get('Resources')}>
-          <Menu.Item key="tutotial">
-            <a target="_blank" rel="noopener noreferrer" href="/tutotial">
-              {intl.get('CheckTutorial')}
-            </a>
-          </Menu.Item>
-          <Menu.Item key="q&a">
-            <a target="_blank" rel="noopener noreferrer" href="/qa">
-              {intl.get('Q&A')}
-            </a>
-          </Menu.Item>
-          <Menu.Item key="privacyTips">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://medium.com/suterusu/privacy-tips-for-suterusu-shield-user-96496bb81447"
-            >
-              {intl.get('PrivacyTips')}
-            </a>
-          </Menu.Item>
-          <Menu.Item key="about">
-            <a target="_blank" rel="noopener noreferrer" href="#">
-              {intl.get('About')}
-            </a>
-          </Menu.Item>
-        </Menu.ItemGroup>
+        <Menu.Item key="info">
+          <a target="_blank" rel="noopener noreferrer" href="/info">
+            {intl.get('Info')}
+          </a>
+        </Menu.Item>
       </Menu>
     );
 
@@ -172,8 +142,23 @@ class Portal extends Component {
               </a>
               <ul className="item-ul">
                 <li>
-                  <a href="/qa" target="_blank">
-                    {intl.get('Q&A')}
+                  <a href="/transfer" target="_blank">
+                    {intl.get('Transfer')}
+                  </a>
+                </li>
+                <li>
+                  <a href="/mining" target="_blank">
+                    {intl.get('Mining')}
+                  </a>
+                </li>
+                <li>
+                  <a href="/tutorial" target="_blank">
+                    {intl.get('Tutorial')}
+                  </a>
+                </li>
+                <li>
+                  <a href="/info" target="_blank">
+                    {intl.get('Info')}
                   </a>
                 </li>
               </ul>
@@ -182,7 +167,7 @@ class Portal extends Component {
           {
             <div className="header-btn">
               <Button className="connectWalletBtn" shape="round">
-                {`Launch App`}
+                <a href="/launch">{intl.get('LaunchApp')}</a>
               </Button>
               <div className="top-btn">
                 <i
@@ -211,18 +196,15 @@ class Portal extends Component {
           <div className="portal-body">
             <div className="portal-section">
               <div className="flex-item">
-                <div className="slogan">
-                  Anonymous PayPal for Cryptocurrency
-                </div>
-                <div className="description">
-                  Suter Shield allows the users to enjoy the amazing private
-                  payment functionality provided by the Suterusu protocol. The
-                  Suterusu protocol is built upon our original ZK-ConSNARK
-                  technology, which strikes a perfect balance between
-                  performance and security.
-                </div>
+                <div className="slogan">{intl.get('Slogan1')}</div>
+                <div
+                  className="description"
+                  dangerouslySetInnerHTML={{
+                    __html: intl.get('SloganDescription'),
+                  }}
+                ></div>
                 <Button className="connectWalletBtn" shape="round" size="large">
-                  Transfer Anonymous
+                  {intl.get('TransferAnonymous')}
                 </Button>
               </div>
               <div className="flex-item">
@@ -230,72 +212,57 @@ class Portal extends Component {
               </div>
             </div>
             <div className="portal-section">
-              <div className="section-title">How Suter Shield works</div>
+              <div
+                className="section-title"
+                dangerouslySetInnerHTML={{ __html: intl.get('Slogan2') }}
+              ></div>
               <div className="flex-item-wrapper">
                 <div className="flex-item">
                   <div className="image-wrapper">
-                    <div className="text">Register</div>
+                    <div className="text">{intl.get('Register')}</div>
                     <img
                       src={require('../static/section-register.svg')}
                       alt="Register"
                     />
                   </div>
                   <div className="description">
-                    The user can register a new Suter account by input a random
-                    private key. The system will generate a public key from this
-                    private key, which will used to identify the newly created
-                    Suter account.
+                    {intl.get('RegisterDescription')}
                   </div>
                 </div>
                 <div className="flex-item">
                   <div className="image-wrapper">
-                    <div className="text">Fund</div>
+                    <div className="text">{intl.get('Fund')}</div>
                     <img
                       src={require('../static/section-fund.svg')}
                       alt="Register"
                     />
                   </div>
                   <div className="description">
-                    The user invokes the fund module and converts ETH or ERC-20
-                    to Suter-ETH or Suter-ERC-20 token. The converted token will
-                    be added to the new account using homomorphic public-key
-                    encryption. The native token will be sent to the fund
-                    contract, which would be like a stream of water entering an
-                    ocean.
+                    {intl.get('FundDescription')}
                   </div>
                 </div>
                 <div className="flex-item">
                   <div className="image-wrapper">
-                    <div className="text">Transfer</div>
+                    <div className="text">{intl.get('Transfer')}</div>
                     <img
                       src={require('../static/section-transfer.svg')}
                       alt="Register"
                     />
                   </div>
                   <div className="description">
-                    The user can then select a receiver Suter account to send
-                    the converted token. This receiver Suter account can be
-                    either generated by himself or sent from the others. The
-                    transfer of the converted token will be anonymous and
-                    confidential since we will invoke the ZK-ConSNARK technique
-                    to guarantee the Suter account anonymity and the
-                    confidentiality of the transferred amount.
+                    {intl.get('TransferDescription')}
                   </div>
                 </div>
                 <div className="flex-item">
                   <div className="image-wrapper">
-                    <div className="text">Burn</div>
+                    <div className="text">{intl.get('Burn')}</div>
                     <img
                       src={require('../static/section-burn.svg')}
                       alt="Register"
                     />
                   </div>
                   <div className="description">
-                    The user can then invoke the burn module and convert the
-                    Suter-token back to its original form. As long as the user
-                    uses a different Ethereum account to invoke this module from
-                    the fund module, the connection between the funded token and
-                    the burned token is severed.{' '}
+                    {intl.get('BurnDescription')}
                   </div>
                 </div>
               </div>
@@ -305,23 +272,14 @@ class Portal extends Component {
                 <img src={require('../static/slogan1.svg')} alt="slogan" />
               </div>
               <div className="flex-item">
-                <div className="slogan">
-                  Why Suter Shield has better Privacy Protection
-                </div>
+                <div
+                  className="slogan"
+                  dangerouslySetInnerHTML={{ __html: intl.get('Slogan3') }}
+                ></div>
                 <div className="description">
-                  The connection between Suter account and Ethereum address is
-                  severed due to the existence of the fund contract. When the
-                  attacker traces the payment records of Ethereum , it would be
-                  like tracing the direction of a small stream of water, you
-                  cannot predict where the stream goes when it reaches the
-                  ocean.
+                  {intl.get('Slogan3Description1')}
                   <div className="cutline"></div>
-                  The miners might still be able to observe the payment records
-                  between different Suter accounts when they verify the
-                  transactions. Homomorphic public key encryption is adopted to
-                  ensure the transferred amount confidentiality. We apply
-                  zero-knowledge proof to guarantee the anonymities of the Suter
-                  accounts.
+                  {intl.get('Slogan3Description2')}
                 </div>
               </div>
             </div>
@@ -329,21 +287,21 @@ class Portal extends Component {
               <div className="flex-item">
                 <div className="card-item">
                   <div className="amount">$12312.12</div>
-                  <div className="title">Total Fund</div>
+                  <div className="title">{intl.get('TotalFund')}</div>
                   <div className="subtitle">subtite here</div>
                 </div>
               </div>
               <div className="flex-item">
                 <div className="card-item">
                   <div className="amount">$12312.12</div>
-                  <div className="title">Total Count</div>
+                  <div className="title">{intl.get('UsageCount')}</div>
                   <div className="subtitle">subtite here</div>
                 </div>
               </div>
               <div className="flex-item">
                 <div className="card-item">
                   <div className="amount">$12312.12</div>
-                  <div className="title">Fee Pool</div>
+                  <div className="title">{intl.get('FeePool')}</div>
                   <div className="subtitle">subtite here</div>
                 </div>
               </div>
