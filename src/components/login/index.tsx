a61c3d52d947f085ef3ec2de410928a5725d2e24
bocai <woaichibocai_hxb@163.com>
2021-01-25 18:35:51 +0800

    login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -4,19 +4,63 @@ import { Client } from '../tools';
 import Web3 from 'web3';
 import openEye from '../../static/openEye.svg';
 import closeEye from '../../static/closeEye.svg';
+import loginLogo from '../../static/login.svg';
 import './index.less';
 
 var Contract = require('web3-eth-contract');
 class Login extends React.Component {
+  state = {
+    inputValue: '',
+    toggleShowPrivateKey: false
+  };
+
   constructor(props) {
     super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.toggleShowPrivateKey = this.toggleShowPrivateKey.bind(this);
   }
+
+  handleInputChange(e) {
+    let value = e.target.value;
+    this.setState({ inputValue: value});
+  }
+  toggleShowPrivateKey(){
+    this.setState({toggleShowPrivateKey: !this.state.toggleShowPrivateKey})
+  }
+
   render() {
+    let { intl } = this.props;
+    let { inputValue, toggleShowPrivateKey } = this.state;
     return (
       <>
        <div className="loginContainer">
-         <div>left</div>
-         <div>right</div>
+         <div className="left">
+           <h1>Log in</h1>
+           <p>Your Suterusu Account</p>
+           <div className="inputContainer">
+             <input
+                placeholder={intl.get("InsertYourprivatekey")}
+                value={inputValue}
+                type={toggleShowPrivateKey ? "text" : "password"}
+                onChange={this.handleInputChange}
+              /> 
+              <div className="inputAppend">
+                <img src={toggleShowPrivateKey ? closeEye : openEye} onClick={this.toggleShowPrivateKey} />
+              </div>
+           </div>
+           <div className="btnContainer">
+              <Button shape="round" block className="loginBtn">
+                Login
+              </Button>
+            </div>
+            <hr/>
+            <div className="goToRegister">
+              Don't have a account? Register.
+            </div>
+         </div>
+         <div className="right">
+           <img src={loginLogo} />
+         </div>
        </div>
       </>
     );
