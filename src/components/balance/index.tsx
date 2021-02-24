2624c56a33cc3d38037f1507ea74f79e74db652f
bocai <woaichibocai_hxb@163.com>
2021-02-24 23:07:17 +0800

    hotfix mobile

--- src/components/balance/index.tsx
+++ src/components/balance/index.tsx
@@ -3,7 +3,7 @@ import './index.less';
 import { CoinLogoMap } from '../tools';
 import Web3 from 'web3';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Tooltip } from 'antd';
+import { Row, Col, Tooltip } from 'antd';
 
 var Contract = require('web3-eth-contract');
 
@@ -32,14 +32,16 @@ class Balance extends React.Component {
         info.contractABI,
         info.contractAddress,
       );
-      suterShiledTokenContract.setProvider(window.ethereum);
+      suterShiledTokenContract.setProvider(
+        new Web3.providers.HttpProvider(JSONRPC_URL),
+      );
       // console.log(suterShiledTokenContract)
       let balanceWithDecimal = await suterShiledTokenContract.methods
         .balanceOf(account)
         .call();
       balance = (balanceWithDecimal * 1.0) / 10 ** info.decimal;
     } else {
-      let newWeb3 = new Web3(window.ethereum);
+      let newWeb3 = new Web3(new Web3.providers.HttpProvider(JSONRPC_URL));
       let balanceWithDecimal = await newWeb3.eth.getBalance(account);
       balance = newWeb3.utils.fromWei(balanceWithDecimal, 'ether');
     }
@@ -68,69 +70,81 @@ class Balance extends React.Component {
     let info = CoinInfos[coinType];
     let { balance, suterShieldBalance } = this.state;
     return (
-      <div className="balanceContainer">
-        <div>
-          <h1>
-            {info.suterShiledBalanceDesc} {intl.get('Balance')}
-          </h1>
-          <div className="balanceInfoContainer">
-            <div>
-              <img src={CoinLogoMap[coinType][1]} />
-            </div>
-            <div className="info">
-              <h2>{suterShieldBalance.toLocaleString()}</h2>
-              <p className="value">
-                {info.valueDesc}(=
-                {(
-                  (suterShieldBalance * 1.0 * info.suterShieldUnit) /
-                  10 ** info.decimal
-                ).toLocaleString()}{' '}
-                {info.unit})
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <h1>
-            {info.coinBalanceDesc} {intl.get('Balance')}
-          </h1>
-          <div className="balanceInfoContainer">
-            <div>
-              <img src={CoinLogoMap[coinType][2]} />
-            </div>
-            <div className="info">
-              <h2>{balance.toLocaleString()} Unit</h2>
-              <p className="value">
-                {info.unit}(=
-                {(
-                  (balance * info.suterShieldUnit * 1.0) /
-                  10 ** info.decimal
-                ).toLocaleString()}{' '}
-                {info.unit})
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="three">
-          <div className="twoLogo">
-            <img src={CoinLogoMap[coinType][2]} />
-            <img src={CoinLogoMap[coinType][1]} className="ml-1" />
-          </div>
-          <div className="unit">
+      <Row className="balanceContainer" align="top" justify="space-between">
+        <Col
+          xs={24}
+          sm={24}
+          md={12}
+          lg={8}
+          xl={6}
+          className="suterShieldBalance"
+        >
+          <div>
             <h1>
-              {this.transformRate(info.suterShieldUnit, 10 ** info.decimal)}
+              {info.suterShiledBalanceDesc} {intl.get('Balance')}
             </h1>
-            <p>Unit</p>
-            <Tooltip placement="topLeft" title={intl.get('i')}>
-              <ExclamationCircleOutlined className="i" />
-            </Tooltip>
+            <div className="balanceInfoContainer">
+              <div>
+                <img src={CoinLogoMap[coinType][1]} />
+              </div>
+              <div className="info">
+                <h2>{suterShieldBalance.toLocaleString()}</h2>
+                <p className="value">
+                  {info.valueDesc}(=
+                  {(
+                    (suterShieldBalance * 1.0 * info.suterShieldUnit) /
+                    10 ** info.decimal
+                  ).toLocaleString()}{' '}
+                  {info.unit})
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="pcLogo">
-          <img src={CoinLogoMap[coinType][0]} />
-        </div>
-      </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6} className="coinBalance">
+          <div>
+            <h1>
+              {info.coinBalanceDesc} {intl.get('Balance')}
+            </h1>
+            <div className="balanceInfoContainer">
+              <div>
+                <img src={CoinLogoMap[coinType][2]} />
+              </div>
+              <div className="info">
+                <h2>{balance.toLocaleString()} Unit</h2>
+                <p className="value">
+                  {info.unit}(=
+                  {(
+                    (balance * info.suterShieldUnit * 1.0) /
+                    10 ** info.decimal
+                  ).toLocaleString()}{' '}
+                  {info.unit})
+                </p>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+          <div className="three">
+            <div className="twoLogo">
+              <img src={CoinLogoMap[coinType][2]} />
+              <img src={CoinLogoMap[coinType][1]} className="ml-1" />
+            </div>
+            <div className="unit">
+              <h1>
+                {this.transformRate(info.suterShieldUnit, 10 ** info.decimal)}
+              </h1>
+              <p>Unit</p>
+              <Tooltip placement="topLeft" title={intl.get('i')}>
+                <ExclamationCircleOutlined className="i" />
+              </Tooltip>
+            </div>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={8} xl={6} className="logo">
+          <img src={CoinLogoMap[coinType][0]} style={{ float: 'right' }} />
+        </Col>
+      </Row>
     );
   }
 }
